7c9258699c02fb361eec4e2dd4010910be94cf88
spencerwhitaker <54879176+spencerwhitaker@users.noreply.github.com>
2019-10-28 13:50:44 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220,220,220);
+  fill(255,218,185);
+  ellipse(50,200,50,100);
+  ellipse(350,200,50,100);
+  circle(200,200,300);
+  fill(255,255,255);
+  ellipse(140,175,80,60);
+  ellipse(260,175,80,60);
+  fill(0,100,19);
+  circle(140,175,30);
+  circle(260,175,30);
+  fill(0,0,0)
+  circle(260,175,10);
+  circle(140,175,10);
+  fill(255,210,190);
+  triangle(175,240,200,165,225,240);
+  fill(55,27,7);
+  point(100,220);
+  point(123,215);
+  point(130,240);
+  point(142,255);
+  point(160,235);
+  point(135,224);
+  point(275,230);
+  point(265,242);
+  point(250,230);
+  point(247,247);
+  point(269,238);
+  point(255,215);
+  point(239,235);
+  fill(255,255,255);
+  quad(140,270,260,270,245,300,155,300);
+  fill(0,0,0);
+  line(148,285,252,285);
+  line(200,270,200,300);
+  line(175,270,175,300);
+  line(225,270,225,300);
+  line(152,270,152,293);
+  line(248,270,248,293);
+  fill(125,0,0);
+  rect(100,25,200,50);
+  rect(60,60,280,70);
+  fill(255,255,255)
+  textSize(28)
+  text('Number 1 Griz Fan',82,105)
+  textSize(16)
+  fill(0,0,0)
+  text('Spencer Whitaker',250,375)
+  
+}
